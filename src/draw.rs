85f05d26a7942bc0d86487535eebb2807d981aca
zhengjing <kei@wlblock.io>
2020-12-01 09:45:57 +0800

    first

---
+++ src/draw.rs
@@ -0,0 +1,43 @@
+use piston_window::{rectangle, Context, G2d};
+use piston_window::types::Color;
+
+const BLACK_SIZE: f64 = 25.0;
+
+pub fn to_coord(game_coord: i32) ->  f64 {
+    (game_coord as f64) * BLACK_SIZE
+}
+
+pub fn to_coord_u32(game_coord: i32) -> u32 {
+    to_coord(game_coord) as u32
+}
+
+pub fn draw_block(color: Color, x: i32,y: i32, con: &Context, g: &mut G2d) {
+    let gui_x = to_coord(x);
+    let gut_y = to_coord(y);
+
+    rectangle(
+        color,
+        [gui_x, gut_y, BLACK_SIZE, BLACK_SIZE],
+        con.transform,
+        g
+    );
+}
+
+pub fn draw_rectangle(
+    color: Color,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+    con: &Context,
+    g: &mut G2d
+) {
+    let x = to_coord(x);
+    let y = to_coord(y);
+    rectangle(
+        color,
+        [x, y, BLACK_SIZE * (width as f64), BLACK_SIZE * (height as f64)],
+        con.transform,
+        g
+    );
+}
